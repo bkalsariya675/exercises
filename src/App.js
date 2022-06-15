dd99ba138fd4250ddf54888ee6c6f94ec41cb56a
bkalsariya675 <58739155+bkalsariya675@users.noreply.github.com>
2022-06-15 11:40:12 +0530

    Responsive Navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import "./styles.css";
-
 export default function App() {
+  const navLinks = document.getElementsByClassName("nav-links")[0];
   return (
-    <div className="search-bar">
-      <label for="search"></label>
-      <input
-        type="text"
-        name="search"
-        id="search"
-        placeholder="search for items..."
-      ></input>
-      <img
-        src="https://img.icons8.com/material-outlined/20/search.pnghttps://img.icons8.com/material-outlined/20/search.png"
-        alt=""
-      />
+    <div className="nav">
+      <div className="brand-title">Headstrait</div>
+      <a
+        href="#toggle"
+        className="toggle-button"
+        onClick={() => {
+          navLinks.classList.toggle("active");
+        }}
+      >
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </a>
+      <div className="nav-links">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
